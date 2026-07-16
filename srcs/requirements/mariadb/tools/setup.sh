28cc2bb2e3cd7638bb2cf27dc0398130287a19c2
youssefnjm <youssefnoujoum04@gmail.com>
2026-07-16 11:49:58 +0100

    add volums and fix second run err

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -5,22 +5,22 @@ mkdir -p /run/mysqld
 chown -R mysql:mysql /run/mysqld
 
 # Check if already initialized 
-# if [ ! -d /var/lib/mysql/mysql ]; then
-    echo "First run: initializing MariaDB..."
+if [ ! -d /var/lib/mysql/mysql ]; then
+    echo "🟡 First run: initializing MariaDB..."
 
     # Create system tables 
-    # mariadb-install-db --user=mysql --datadir=/var/lib/mysql
+    mariadb-install-db --user=mysql --datadir=/var/lib/mysql
 
     # Start server temporarily in background 
     mysqld --user=mysql --datadir=/var/lib/mysql &
 
     # Wait until server is ready 
-    echo "Waiting for MariaDB to start..."
+    echo "🟡 Waiting for MariaDB to start..."
     until mysqladmin ping --silent; do
-        echo "MariaDB not ready yet. Retrying..."
+        echo "🟡 MariaDB not ready yet. Retrying..."
         sleep 1
     done
-    echo "MariaDB is ready!"
+    echo "🟡 MariaDB is ready!"
 
     # Create database and users 
     mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
@@ -28,11 +28,11 @@ chown -R mysql:mysql /run/mysqld
     mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO '$MYSQL_USER'@'%';"
     mysql -u root -e "FLUSH PRIVILEGES;"
 
-    echo "Database and users created."
+    echo "🟡 Database and users created."
 
     # Stop temporary server 
     kill $!
-# fi
+fi
 
 # Start server in foreground as PID 1 
 echo "🟢 Starting MariaDB in foreground..."
--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -5,36 +5,36 @@ set -e
 # mkdir -p /var/www/html
 mkdir -p /run/php
 
-echo "Waiting for MariaDB..."
-until mysqladmin ping -h mariadb -u $MYSQL_USER -$MYSQL_PASSWORD --silent; do
+echo "🟡 Waiting for MariaDB..."
+until mysqladmin ping -h mariadb -u $MYSQL_USER -p$MYSQL_PASSWORD --silent; do
     echo "MariaDB not ready yet. Retrying..."
     sleep 1
 done
-echo "MariaDB is ready!"
+echo "🟡 MariaDB is ready!"
 
 # Download WordPress if not present
 if [ ! -f wp-config.php ] && [ ! -f wp-config-sample.php ]; then
-    echo "Downloading WordPress..."
+    echo "🟡 Downloading WordPress..."
     wp core download --allow-root
 else
-    echo "wordpress is file already present";
+    echo "🟡 wordpress is file already present";
 fi
 
 # Create wp-config.php if not present
 if [ ! -f wp-config.php ]; then
-    echo "Creating wp-config.php..."
+    echo "🟡 Creating wp-config.php..."
     wp config create --allow-root \
         --dbname=$MYSQL_DATABASE \
         --dbuser=$MYSQL_USER \
         --dbpass=$MYSQL_PASSWORD \
         --dbhost=$DB_HOST
 else
-    echo "wp-config.php file already exist";
+    echo "🟡 wp-config.php file already exist";
 fi
 
 # Install WordPress if not already installed
 if ! wp core is-installed --allow-root ; then
-    echo "Installing WordPress..."
+    echo "🟡 Installing WordPress..."
     wp core install --allow-root \
         --url="ynoujoum.42.fr" \
         --title="Inception" \
@@ -43,17 +43,16 @@ if ! wp core is-installed --allow-root ; then
         --admin_email=$WP_ADMIN_EMAIL \
         --skip-email
 
-    echo "Creating second user..."
+    echo "🟡 Creating second user..."
     wp user create --allow-root \
         $WP_USER $WP_USER_EMAIL \
         --user_pass=$WP_USER_PASS \
         --role=editor
 else
-    echo "wordpress file already present";
+    echo "🟡 wordpress file already present";
 fi
 
 # Copy test PHP file and set files owner
-cp /usr/local/bin/index.php /var/www/html/index.php 2>/dev/null || true
 chown -R www-data:www-data /var/www/html
 
 echo "🟢 Starting worldpress in foreground..."
